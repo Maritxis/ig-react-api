c5a7eb7e8f59c8cf2660f48736c47cd9fe6b1f1d
Henar Hernández <hhernandez@eldiario.es>
2020-02-21 13:31:24 +0100

    [master] temp

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <button>Home</button>
+      <button>Cancel</button>
+      <button>Next</button>
+      <button>Share</button>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/containers/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import Container from '../../components/container/Container';
+
+const Home = () => {
+  return (
+    <>
+      <Header></Header>
+      <Container></Container>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Home;
